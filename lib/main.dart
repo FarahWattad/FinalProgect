ba9bf5103fb5b301f7d942be71eff52bc4dc261e
uploadyogithub <farah.wattad2007@gmail.com>
2024-10-15 12:05:57 +0300

    button

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -21,6 +19,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -41,26 +40,52 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Email :" , style:TextStyle( fontSize : 20) ,),
-
-            Text("Password :" , style:TextStyle( fontSize : 20) ,),
-            Text("City :" , style:TextStyle( fontSize : 20) ,),
-
-    
+            Text(
+              "Email :",
+              style: TextStyle(fontSize: 20),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter Email',
+              ),
+            ),
+            Text(
+              "Password :",
+              style: TextStyle(fontSize: 20),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter Password',
+              ),
+            ),
+            Text(
+              "City :",
+              style: TextStyle(fontSize: 20),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter City',
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () {},
+              child: Text('Next'),
+            )
           ],
         ),
       ),
