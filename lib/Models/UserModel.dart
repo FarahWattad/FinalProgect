4aa17f5354255203b94fdf326a913365e8ffa2a3
uploadyogithub <farah.wattad2007@gmail.com>
2024-10-26 11:32:15 +0300

    UserMV

--- lib/Models/UserModel.dart
+++ lib/Models/UserModel.dart
@@ -4,23 +4,30 @@ class User{
     this.name="",
     this.phone="",
     this.note="",
+    this.Address="",
+
   });
 
   String id;
   String name;
   String note;
   String phone;
+  String Address;
 
   factory User.fromJson(Map<String,dynamic> json)=>User(
     id: json ["id"],
     name: json["name"],
     phone: json["phone"],
     note: json["note"],
+    Address: json["Address"],
+
   );
   Map<String,dynamic> toJson() => {
     "id": id,
     "name": name,
     "phone":phone,
     "note":note,
+    "Address":Address,
+
   };
 }
